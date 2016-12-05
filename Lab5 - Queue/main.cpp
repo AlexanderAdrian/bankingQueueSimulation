1d5120a25d71ce44134fa1ce8e2beb06a3c6448c
Alexander Cardozo <alexander.a.cardozo@gmail.com>
2016-12-05 17:27:54 -0500

    Initial Commit

---
+++ Lab5 - Queue/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab5 - Queue
+//
+//  Created by Alexander Adrian Cardozo on 12/5/16.
+//  Copyright © 2016 Alexander Adrian Cardozo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
